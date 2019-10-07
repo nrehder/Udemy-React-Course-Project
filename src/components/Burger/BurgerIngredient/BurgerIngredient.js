4648a34bb2d323f43d0b9ff96441f611b8299958
nrehder <nrehder@gmail.com>
2019-10-07 14:35:30 -0400

    set up basic burger

---
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+import classes from "./BurgerIngredient.module.css";
+
+class BurgerIngredient extends Component {
+	render() {
+		switch (this.props.type) {
+			case "bread-bottom":
+				return <div className={classes.BreadBottom}></div>;
+			case "bread-top":
+				return (
+					<div className={classes.BreadTop}>
+						<div className={classes.Seeds1}></div>
+						<div className={classes.Seeds2}></div>
+					</div>
+				);
+			case "meat":
+				return <div className={classes.Meat}></div>;
+			case "cheese":
+				return <div className={classes.Cheese}></div>;
+			case "lettuce":
+				return <div className={classes.Lettuce}></div>;
+			case "bacon":
+				return <div className={classes.Bacon}></div>;
+			default:
+				return null;
+		}
+	}
+}
+
+BurgerIngredient.propTypes = {
+	type: PropTypes.string.isRequired,
+};
+
+export default BurgerIngredient;
